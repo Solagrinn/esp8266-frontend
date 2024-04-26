43eaae09b2a2d32a2fca99caf597ae1007e2f7a9
Solagrinn <eraydindar508@gmail.com>
2024-04-26 11:11:05 +0300

    Test 1

--- src/Components/Advanced/ChainLatencyCard.tsx
+++ src/Components/Advanced/ChainLatencyCard.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const ChainLatencyCard = () => {
     return (
         <div>
-
+            This is a test change to see if this shows up on github.
 
         </div>
     )
